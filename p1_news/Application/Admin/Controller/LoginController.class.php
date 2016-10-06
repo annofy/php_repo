d5d56d2f030af63509bcbd271e1163c575ec72fb
annofy <zhenglfsir@gmail.com>
2016-10-06 10:56:09 +0800

    配置资源文件

---
+++ p1_news/Application/Admin/Controller/LoginController.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhenglfsir
+ * Date: 2016/10/6
+ * Time: 10:47
+ */
+namespace Admin\Controller;
+use Think\Controller;
+
+class LoginController extends Controller
+{
+    public function index() {
+        $this->display();
+    }
+}
